583459d6e070270d0e8709a4493240e9899ff5a9
setsiri <76035222+setsiri@users.noreply.github.com>
2021-01-12 19:30:30 +0700

    finit

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,21 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+    string x[]={"A","+B","B","+c","C","D","F"};
+    srand(time(0));
+    cout<<"Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    int i = rand()%9;
+    cout<<"You will get "<<x[i]<<" in this 261102.";
+    return 0;
+}
+
+
+
+
